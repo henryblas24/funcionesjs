b713f6db6e01ff412332923518b8ff72fa887788
henry blas <hblas2014@gmail.com>
2023-05-10 19:03:59 -0500

    se realizo trabajo de menu de funciones

---
+++ app.js
@@ -0,0 +1,33 @@
+//*var miArreglo = [[1, 2, 3, 4],[5, 6, 7, //8],[9, 10, 11, 12]];
+
+//for ( let i = 0; i < miArreglo.length; i++ ) {
+//    console.log('empieza iteracion')
+//  console.log('la longitud de mi arreglo es ' + miArreglo.length)
+//    let arregloAnidado = miArreglo[i];
+//    console.log(miArreglo[i]);
+//   console.log('arreglo anidado ' + arregloAnidado);
+//   let suma = 0;
+//    for ( let j = 0; j < arregloAnidado.length; j++) {
+//       console.log('la longitud de mi arreglo anidado es ' + arregloAnidado.length);
+//      console.log(arregloAnidado[j]);
+//        suma = suma + arregloAnidado[j];
+//    }
+//    console.log(suma);  
+//};
+//console.log('hola mundo')
+//console.log(suma) *//
+let n = 30 ;
+while n<=30 {
+  n++  
+  if n % == o
+
+
+} 
+  
+
+
+
+
+
+
+
