b09845432c881f11280cc68184b6c63e1658963a
henry blas <hblas2014@gmail.com>
2023-05-18 04:58:09 -0500

    se realizo ejercicios de eventos

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-//*var miArreglo = [[1, 2, 3, 4],[5, 6, 7, //8],[9, 10, 11, 12]];
-
-//for ( let i = 0; i < miArreglo.length; i++ ) {
-//    console.log('empieza iteracion')
-//  console.log('la longitud de mi arreglo es ' + miArreglo.length)
-//    let arregloAnidado = miArreglo[i];
-//    console.log(miArreglo[i]);
-//   console.log('arreglo anidado ' + arregloAnidado);
-//   let suma = 0;
-//    for ( let j = 0; j < arregloAnidado.length; j++) {
-//       console.log('la longitud de mi arreglo anidado es ' + arregloAnidado.length);
-//      console.log(arregloAnidado[j]);
-//        suma = suma + arregloAnidado[j];
-//    }
-//    console.log(suma);  
-//};
-//console.log('hola mundo')
-//console.log(suma) *//
-let n = 30 ;
-while n<=30 {
-  n++  
-  if n % == o
+//  Ejercicio 1 :
+//  Este programa es buscar un nombre dentro del array y si no se 
+//  encuentra dentro de la lista del array, emitira un mensaje que
+//  existe o no existe.
+//  
 
 
-} 
-  
+/*const arrayName = ['Diego','jose','Pedro'];
+const question = prompt('ingrese su nombre');
 
+const condition = arrayName.includes(question);
+
+if (condition) {
+    console.log('Si EXISTE en la BD');
+ } else {
+    console.log('NO EXISTE en la BD');
+ };*/
+   
+/*condition ? console.log('Si existe en la Base de Datos') : console.log('No existe en la Base de Datos');*/
+
+    
+    
+//  Ejercicio 2 :
+//  Este programa es para validar un nombre de una capital, si es correcto 
+//  emitira una alerta (excelente),sino emitira un mensaje alerta (No es la 
+//  respuesta.
+//
+
+/*const btn = document.getElementById('boton');
+console.log(btn);
+
+const pregunta = prompt ('Ingrese el nombre de la capital de Perú');
+if (pregunta === "Lima") {
+    alert('EXCELENTE')
+} else {
+    alert('No es la respuesta')
+};
+
+boton.addEventListener('click','pregunta'); */
+
+
+
+//  Ejercicio 3 :
+//  Este programa es permite mostrar las propiedades de los eventos 
+//  por medio de un click del boton "Haz click"  y a la vez permite ocultar
+//  el boton por medio de evento con su propiedad y estilo de css.
+//
+
+const btn = document.getElementById('boton');
+
+const callConsole = (e) => {
+     console.log('event', e);
+     console.log('target', e.target);
+     e.target.style = " display : none "
+     console.log('type', e.type);
+};
+btn.addEventListener('click',callConsole);
 
 
 
